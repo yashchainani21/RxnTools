c1bd60cf2ebb2840c45831f28b88f0dd68334639
yashchainani21 <yashchainani2026@u.northwestern.edu>
2024-12-23 10:11:35 -0800

    added tests to locate all changed atoms

--- tests/test_reaction.py
+++ tests/test_reaction.py
@@ -31,7 +31,7 @@ def test_get_mapped_bonds_data_type():
         bond_type = bond[2]
         assert isinstance(bond_type, Chem.rdchem.BondType)
 
-def test_get_mapped_bonds_ethanol():
+def test_get_mapped_bonds_frm_ethanol_SMARTS():
     """
     Test to ensure the internal function _get_mapped_bonds returns all atom-mapped bonds.
     The start index, end index, as well as bond type should be correctly identified.
@@ -44,7 +44,7 @@ def test_get_mapped_bonds_ethanol():
     assert (1, 2, Chem.rdchem.BondType.SINGLE) in mapped_bonds # ensure C-C bond is present
     assert (2, 3, Chem.rdchem.BondType.SINGLE) in mapped_bonds # ensure C-O bond is present
 
-def test_get_mapped_bonds_ethanal():
+def test_get_mapped_bonds_frm_ethanal_SMARTS():
     """
     Test to ensure the internal function _get_mapped_bonds returns all atom-mapped bonds.
     The start index, end index, as well as bond type should be correctly identified.
@@ -57,7 +57,7 @@ def test_get_mapped_bonds_ethanal():
     assert (1, 2, Chem.rdchem.BondType.SINGLE) in mapped_bonds  # ensure C-C bond is present
     assert (2, 3, Chem.rdchem.BondType.DOUBLE) in mapped_bonds  # ensure C=O bond is present
 
-def test_get_mapped_bonds_nad_plus():
+def test_get_mapped_bonds_frm_nad_plus_SMARTS():
     """
     Test to ensure the internal function _get_mapped_bonds returns all atom-mapped bonds.
     The start index, end index, as well as bond type should be correctly identified.
@@ -128,7 +128,7 @@ def test_get_mapped_bonds_nad_plus():
     assert (36, 37, Chem.rdchem.BondType.AROMATIC) in mapped_bonds
     assert (37, 38, Chem.rdchem.BondType.AROMATIC) in mapped_bonds
 
-def test_get_mapped_bonds_nadh_plus():
+def test_get_mapped_bonds_frm_nadh_plus_SMARTS():
     """
     Test to ensure the internal function _get_mapped_bonds returns all atom-mapped bonds.
     The start index, end index, as well as bond type should be correctly specified.
@@ -199,4 +199,39 @@ def test_get_mapped_bonds_nadh_plus():
     assert (36, 37, Chem.rdchem.BondType.AROMATIC) in mapped_bonds
     assert (37, 38, Chem.rdchem.BondType.AROMATIC) in mapped_bonds
 
+def test_get_all_changed_atoms_frm_ethanol_AdH_rxn_SMARTS():
+    mapped_rxn = reaction.mapped_reaction(
+        '[CH3:1][CH2:2][OH:3].[NH2:4][C:5](=[O:6])[c:7]1[cH:8][cH:9][cH:10][n+:11]([C@@H:12]2[O:13][C@H:14]([CH2:15][O:16][P:17](=[O:18])([OH:19])[O:20][P:21](=[O:22])([OH:23])[O:24][CH2:25][C@H:26]3[O:27][C@@H:28]([n:29]4[cH:30][n:31][c:32]5[c:33]([NH2:34])[n:35][cH:36][n:37][c:38]45)[C@H:39]([OH:40])[C@@H:41]3[OH:42])[C@@H:43]([OH:44])[C@H:45]2[OH:46])[cH:47]1>>[CH3:1][CH:2]=[O:3].[H+].[NH2:4][C:5](=[O:6])[C:7]1=[CH:47][N:11]([C@@H:12]2[O:13][C@H:14]([CH2:15][O:16][P:17](=[O:18])([OH:19])[O:20][P:21](=[O:22])([OH:23])[O:24][CH2:25][C@H:26]3[O:27][C@@H:28]([n:29]4[cH:30][n:31][c:32]5[c:33]([NH2:34])[n:35][cH:36][n:37][c:38]45)[C@H:39]([OH:40])[C@@H:41]3[OH:42])[C@@H:43]([OH:44])[C@H:45]2[OH:46])[CH:10]=[CH:9][CH2:8]1')
 
+    changed_atoms, broken_bonds, formed_bonds = mapped_rxn._get_all_changed_atoms()
+
+    # check first if the data types returned are as expected
+    assert isinstance(changed_atoms, set)
+    assert isinstance(broken_bonds, set)
+    assert isinstance(formed_bonds, set)
+
+    # ensure correct atom indices corresponding to transformed atoms are in changed_atoms
+    expected_atoms_changed = [2, 3, 7, 8, 9, 10, 11, 47]
+    assert all(item in changed_atoms for item in expected_atoms_changed)
+
+    # ensure that a C-O single bond has been broken within ethanol (CCO)
+    assert (2, 3, Chem.rdchem.BondType.SINGLE) in broken_bonds
+
+    # ensure that a C=O double bond has been formed within ethanal (CC=O)
+    assert (2, 3, Chem.rdchem.BondType.DOUBLE) in formed_bonds
+
+    # ensure that the aromaticity of the pyridine is broken as expected in an AdH reaction
+    assert (7, 8, Chem.rdchem.BondType.AROMATIC) in broken_bonds
+    assert (8, 9, Chem.rdchem.BondType.AROMATIC) in broken_bonds
+    assert (9, 10, Chem.rdchem.BondType.AROMATIC) in broken_bonds
+    assert (10, 11, Chem.rdchem.BondType.AROMATIC) in broken_bonds
+    assert (11, 47, Chem.rdchem.BondType.AROMATIC) in broken_bonds
+    assert (47, 7, Chem.rdchem.BondType.AROMATIC) in broken_bonds
+
+    # ensure that new single and double bonds are formed within the pyrimidine ring
+    assert (8, 7, Chem.rdchem.BondType.SINGLE) in formed_bonds
+    assert (9, 8, Chem.rdchem.BondType.SINGLE) in formed_bonds
+    assert (10, 9, Chem.rdchem.BondType.DOUBLE) in formed_bonds
+    assert (11, 10, Chem.rdchem.BondType.SINGLE) in formed_bonds
+    assert (47, 11, Chem.rdchem.BondType.SINGLE) in formed_bonds
+    assert (7, 47, Chem.rdchem.BondType.DOUBLE) in formed_bonds
