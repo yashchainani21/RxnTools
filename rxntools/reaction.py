001afd3d7f56ca7f19b19e04334c59ef0fc44293
yashchainani21 <yashchainani2026@u.northwestern.edu>
2024-12-30 09:43:56 -0800

    new get_all_changed_atoms function can incl. or excl. cofactors

--- rxntools/reaction.py
+++ rxntools/reaction.py
@@ -1,6 +1,7 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from typing import Set,Tuple
+from .utils import is_cofactor
 
 class unmapped_reaction:
     """
@@ -105,3 +106,104 @@ class mapped_reaction:
             changed_atoms.add(bond[1]) # atom map number of the second atom
 
         return changed_atoms, broken_bonds, formed_bonds
+
+    def get_all_changed_atoms(self,
+                              include_cofactors: bool,
+                              consider_stereo: bool,
+                              cofactors_list: list) -> Tuple[ Set[int],
+                                                              Set[Tuple[int, int, Chem.rdchem.BondType]],
+                                                              Set[Tuple[int, int, Chem.rdchem.BondType]] ]:
+        """
+        Parameters
+        ----------
+        self
+
+        include_cofactors: bool
+            Whether to include transformed cofactor atoms in tracking all changed atoms
+
+        consider_stereo: bool
+            Whether to consider stereochemistry of cofactors specifically
+
+        cofactors_list : list
+            List of cofactor SMILES strings
+
+        Returns
+        -------
+        changed_atoms: Set[int]
+            The set atom indices corresponding to all changed atoms across the reaction.
+
+        include_cofactors: bool
+            Whether to include cofactor atoms or not in determining which atoms are transformed
+
+        cofactors_list: list
+            List of cofactor SMILES
+
+        broken_bonds: Set[Tuple[int, int, Chem.rdchem.BondType]]
+            The set of all bonds that were broken across the reaction.
+            Each element in this set is a tuple comprising three elements.
+            The first two elements are integers indicating start & end atom indices of a bond.
+            The third element corresponds to the bond type, i.e. single, double or aromatic.
+            This bond type is a Chem.rdchem.BondType data type.
+
+        formed_bonds: Set[Tuple[int, int, Chem.rdchem.BondType]]
+            Similar to broken_bonds, but for formed bonds.
+        """
+        # create an RDKit reaction object from a fully atom-mapped reaction smarts string
+        rxn = AllChem.ReactionFromSmarts(self.rxn_smarts)
+        reactants = rxn.GetReactants()
+        products = rxn.GetProducts()
+
+        reactant_bonds = set()
+        product_bonds = set()
+
+        # collect all mapped bonds from all reactants involved
+        for reactant_mol in reactants:
+
+            # if cofactors are being tracked too, store all mapped bonds for all reactants
+            if include_cofactors:
+                reactant_bonds |= self._get_mapped_bonds(mol = reactant_mol)
+
+            # if tracking only changed substrate and product atoms but not cofactor atoms,
+            else:
+                # check if a reactant is a cofactor first and do nothing if this is true
+                if is_cofactor(reactant_mol,
+                                cofactors_list = cofactors_list,
+                                consider_stereo = consider_stereo):
+                    pass
+
+                # if a reactant is not a cofactor, store its mapped bonds
+                else:
+                    reactant_bonds |= self._get_mapped_bonds(mol = reactant_mol)
+
+        # collect all mapped bonds from all products involved
+        for product_mol in products:
+
+            # if cofactors are being tracked too, store all mapped bonds for all products
+            if include_cofactors:
+                product_bonds |= self._get_mapped_bonds(mol = product_mol)
+
+            # if tracking only changed substrate and product atoms but not cofactor atoms,
+            else:
+                # check if a product is a cofactor first and do nothing if this is true
+                if is_cofactor(product_mol,
+                                cofactors_list = cofactors_list,
+                                consider_stereo = consider_stereo):
+                    pass
+
+                # if a product is not a cofactor, store its mapped bonds
+                else:
+                    product_bonds |= self._get_mapped_bonds(mol = product_mol)
+
+        # identify all broken bonds and formed bonds
+        broken_bonds = reactant_bonds - product_bonds # bonds present in reactants but not products
+        formed_bonds = product_bonds - reactant_bonds # bonds present in products but not reactants
+
+        # collect atom mapped numbers involved in broken or formed bonds
+        changed_atoms = set()
+        for bond in broken_bonds.union(formed_bonds):
+            changed_atoms.add(bond[0]) # atom map number of the first atom
+            changed_atoms.add(bond[1]) # atom map number of the second atom
+
+        return changed_atoms, broken_bonds, formed_bonds
+
+
