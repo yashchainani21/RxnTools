bce6a47d1870ef529f8c4f749c0def0560263ac0
yashchainani21 <yashchainani2026@u.northwestern.edu>
2024-12-30 09:41:17 -0800

    test to ensure all cofactors are unique

---
+++ tests/test_cofactors_data.py
@@ -0,0 +1,12 @@
+import json
+
+def test_cofactors_are_unique():
+    with open('../data/cofactors.json', 'r') as f:
+        cofactors_dict = json.load(f)
+
+    cofactors_smiles_list = []
+
+    for key in cofactors_dict.keys():
+        cofactors_smiles_list.append(cofactors_dict[key])
+
+    assert len(set(cofactors_smiles_list)) == len(cofactors_smiles_list)
