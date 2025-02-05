58573809c70d8ed224dbe0f2e400b183ae4fcae4
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-02-05 12:12:44 -0600

    adding tests for the unmapped reaction class

--- tests/test_reaction.py
+++ tests/test_reaction.py
@@ -13,6 +13,12 @@ def cofactors_list():
         cofactors_dict = json.load(f)
     return [cofactors_dict[key] for key in cofactors_dict.keys()]
 
+def test_separating_unmapped_rxn_into_reactant_and_product_strs():
+    rxn = reaction.unmapped_reaction(rxn_str = "CCO.NC(=O)c1ccc[n+]([C@@H]2O[C@H](COP(=O)(O)OP(=O)(O)OC[C@H]3O[C@@H](n4cnc5c(N)ncnc54)[C@H](O)[C@@H]3O)[C@@H](O)[C@H]2O)c1>>CC=O.NC(=O)C1=CN([C@@H]2O[C@H](COP(=O)(O)OP(=O)(O)OC[C@H]3O[C@@H](n4cnc5c(N)ncnc54)[C@H](O)[C@@H]3O)[C@@H](O)[C@H]2O)C=CC1.[H+]")
+    reactants_str, products_str = rxn._rxn_2_cpds()
+    assert reactants_str == "CCO.NC(=O)c1ccc[n+]([C@@H]2O[C@H](COP(=O)(O)OP(=O)(O)OC[C@H]3O[C@@H](n4cnc5c(N)ncnc54)[C@H](O)[C@@H]3O)[C@@H](O)[C@H]2O)c1"
+    assert products_str == "CC=O.NC(=O)C1=CN([C@@H]2O[C@H](COP(=O)(O)OP(=O)(O)OC[C@H]3O[C@@H](n4cnc5c(N)ncnc54)[C@H](O)[C@@H]3O)[C@@H](O)[C@H]2O)C=CC1.[H+]"
+
 def test_get_mapped_bonds_data_type():
     """
     Test to ensure the internal function _get_mapped_bonds returns a set.
