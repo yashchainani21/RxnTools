a5d97e43acebb95a7c0ab627be0a4b320db1f007
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-02-07 18:30:51 -0600

    better cofactor code extraction

--- tests/test_cofactors_data.py
+++ tests/test_cofactors_data.py
@@ -9,14 +9,3 @@ def test_cofactors_are_unique():
         cofactors_smiles_list.append(cofactors_dict[key])
 
     assert len(set(cofactors_smiles_list)) == len(cofactors_smiles_list)
-
-def test_cofactors_with_CoF_codes_are_unique():
-    with open('../data/cofactors_with_CoF_codes.json', 'r') as f:
-        cofactors_dict = json.load(f)
-
-    cofactors_smiles_list = []
-
-    for key in cofactors_dict.keys():
-        cofactors_smiles_list.append(cofactors_dict[key])
-
-    assert len(set(cofactors_smiles_list)) == len(cofactors_smiles_list)
