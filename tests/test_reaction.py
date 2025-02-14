07881533b75e822015aad9035894646b9f83ad5e
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-02-13 22:34:36 -0600

    fix canon, neturalization, and respective tests

--- tests/test_reaction.py
+++ tests/test_reaction.py
@@ -72,11 +72,11 @@ def test_separating_unmapped_rxn_str_into_reactant_and_product_strs_02():
 def test_extracting_substrates_from_unmapped_rxn_str_02(cofactors_list):
     rxn = reaction.unmapped_reaction(rxn_str = hydroxyproline_AdH_rxn_str)
     assert rxn.get_substrates(cofactors_list = cofactors_list,
-                              consider_stereo = False) == ["O=C([C@@H]1C[C@@H](CN1)O)O"]
+                              consider_stereo = False) == ["O=C(O)[C@@H]1C[C@H](O)CN1"]
 
 def test_extracting_products_from_unmapped_rxn_str_02(cofactors_list):
     rxn = reaction.unmapped_reaction(rxn_str = hydroxyproline_AdH_rxn_str)
-    assert rxn.get_products(cofactors_list=cofactors_list, consider_stereo=False) == ["O=C1C[NH2+][C@H](C(=O)[O-])C1"]
+    assert rxn.get_products(cofactors_list=cofactors_list, consider_stereo=False) == ["O=C1CN[C@H](C(=O)O)C1"]
 
 def extracting_LHS_cofactors_from_unmapped_rxn_str_02(cofactors_list):
     rxn = reaction.unmapped_reaction(rxn_str = hydroxyproline_AdH_rxn_str)
