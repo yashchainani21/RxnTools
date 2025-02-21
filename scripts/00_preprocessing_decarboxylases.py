6607c7b2f39f607665d1465d4629338bfae44797
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-02-21 17:43:19 -0600

    extract decarboxylation rxns from brenda/enzymemap

--- scripts/00_preprocessing_decarboxylases.py
+++ scripts/00_preprocessing_decarboxylases.py
@@ -11,7 +11,20 @@ with open('../data/raw/all_cofactors.csv') as f:
 
 brenda_filepath = '../data/raw/enzymemap_v2_brenda2023.csv'
 
-df = pd.read_csv(brenda_filepath)
-for rxn_str in list(df['unmapped'])[-10:-1]:
-    rxn = reaction.unmapped_reaction(rxn_str)
-    print(rxn.get_JN_rxn_descriptor(cofactors_df, consider_stereo = False))
+brenda_df = pd.read_csv(brenda_filepath)
+
+#
+for i, rxn_str in enumerate(list(brenda_df['unmapped'])):
+    reactants, products = rxn_str.split('>>')
+    reactants_list = reactants.split('.')
+    products_list = products.split('.')
+
+    if len(reactants_list) == 1 and len(products_list) == 2:
+
+        for product_smiles in products_list:
+            if product_smiles == 'O=C=O':
+                keep_idx.append(i)
+
+query_df = BRENDA_df.iloc[keep_idx, :]
+query_df = query_df[query_df['source'] == 'direct']
+query_rxns_frm_BRENDA = set(query_df['mapped'])
