110ecfba2299f7ca53537c2614e3e5a34be01048
yashchainani21 <ychainani21@gmail.com>
2025-11-26 18:21:19 -0600

    update reactions

--- tests/test_reaction.py
+++ tests/test_reaction.py
@@ -441,6 +441,8 @@ def test_extracting_rhs_cofactors_from_mapped_rxn_str_01(cofactors_list):
     assert rxn.get_rhs_cofactors(cofactors_list = cofactors_list,
                                  consider_stereo = False) == ["[H+]","[NH2:4][C:5](=[O:6])[C:7]1=[CH:47][N:11]([C@@H:12]2[O:13][C@H:14]([CH2:15][O:16][P:17](=[O:18])([OH:19])[O:20][P:21](=[O:22])([OH:23])[O:24][CH2:25][C@H:26]3[O:27][C@@H:28]([n:29]4[cH:30][n:31][c:32]5[c:33]([NH2:34])[n:35][cH:36][n:37][c:38]45)[C@H:39]([OH:40])[C@@H:41]3[OH:42])[C@@H:43]([OH:44])[C@H:45]2[OH:46])[CH:10]=[CH:9][CH2:8]1"]
 
+# tests involving an alcohol dehydrogenase that oxidizes cis-4-hydroxyproline to the corresponding aldehyde
+# EC 1.1.1.104, MetaCyc rxn idx 693 (4-OXOPROLINE-REDUCTASE-RXN)
 hydroxyproline_AdH_rxn_str_mapped = "[NH2:1][C:2](=[O:3])[c:4]1[cH:5][cH:6][cH:7][n+:8]([C@@H:9]2[O:10][C@H:11]([CH2:12][O:13][P:14](=[O:15])([OH:16])[O:17][P:18](=[O:19])([OH:20])[O:21][CH2:22][C@H:23]3[O:24][C@@H:25]([n:26]4[cH:27][n:28][c:29]5[c:30]([NH2:31])[n:32][cH:33][n:34][c:35]45)[C@H:36]([OH:37])[C@@H:38]3[OH:39])[C@@H:40]([OH:41])[C@H:42]2[OH:43])[cH:44]1.[O:45]=[C:46]([O-:47])[C@@H:48]1[CH2:49][C@H:50]([OH:51])[CH2:52][NH2+:53]1>>[H+].[NH2:1][C:2](=[O:3])[C:4]1=[CH:44][N:8]([C@@H:9]2[O:10][C@H:11]([CH2:12][O:13][P:14](=[O:15])([OH:16])[O:17][P:18](=[O:19])([OH:20])[O:21][CH2:22][C@H:23]3[O:24][C@@H:25]([n:26]4[cH:27][n:28][c:29]5[c:30]([NH2:31])[n:32][cH:33][n:34][c:35]45)[C@H:36]([OH:37])[C@@H:38]3[OH:39])[C@@H:40]([OH:41])[C@H:42]2[OH:43])[CH:7]=[CH:6][CH2:5]1.[O:45]=[C:46]([O-:47])[C@@H:48]1[CH2:49][C:50](=[O:51])[CH2:52][NH2+:53]1"
 
 def test_separating_mapped_rxn_str_into_reactant_and_product_strs_02():
@@ -469,6 +471,33 @@ def test_extracting_rhs_cofactors_from_mapped_rxn_str_02(cofactors_list):
     assert rxn.get_rhs_cofactors(cofactors_list = cofactors_list,
                                  consider_stereo = False) == ["[H+]", "[NH2:1][C:2](=[O:3])[C:4]1=[CH:44][N:8]([C@@H:9]2[O:10][C@H:11]([CH2:12][O:13][P:14](=[O:15])([OH:16])[O:17][P:18](=[O:19])([OH:20])[O:21][CH2:22][C@H:23]3[O:24][C@@H:25]([n:26]4[cH:27][n:28][c:29]5[c:30]([NH2:31])[n:32][cH:33][n:34][c:35]45)[C@H:36]([OH:37])[C@@H:38]3[OH:39])[C@@H:40]([OH:41])[C@H:42]2[OH:43])[CH:7]=[CH:6][CH2:5]1"]
 
+# tests involving an alcohol dehydrogenase that oxidizes mannofuranose to the corresponding aldehyde
+# EC 1.1.1.173, MetaCyc rxn idx 1652 (L-RHAMNOSE-1-DEHYDROGENASE-RXN)
+mannofuranose_AdH_rxn_str_mapped = "[CH3:1][C@H:2]([OH:3])[C@@H:4]1[O:5][CH:6]([OH:7])[C@H:8]([OH:9])[C@@H:10]1[OH:11].[NH2:12][C:13](=[O:14])[c:15]1[cH:16][cH:17][cH:18][n+:19]([C@@H:20]2[O:21][C@H:22]([CH2:23][O:24][P:25](=[O:26])([OH:27])[O:28][P:29](=[O:30])([OH:31])[O:32][CH2:33][C@H:34]3[O:35][C@@H:36]([n:37]4[cH:38][n:39][c:40]5[c:41]([NH2:42])[n:43][cH:44][n:45][c:46]45)[C@H:47]([OH:48])[C@@H:49]3[OH:50])[C@@H:51]([OH:52])[C@H:53]2[OH:54])[cH:55]1>>[CH3:1][C@H:2]([OH:3])[C@@H:4]1[O:5][C:6](=[O:7])[C@H:8]([OH:9])[C@@H:10]1[OH:11].[H+].[NH2:12][C:13](=[O:14])[C:15]1=[CH:55][N:19]([C@@H:20]2[O:21][C@H:22]([CH2:23][O:24][P:25](=[O:26])([OH:27])[O:28][P:29](=[O:30])([OH:31])[O:32][CH2:33][C@H:34]3[O:35][C@@H:36]([n:37]4[cH:38][n:39][c:40]5[c:41]([NH2:42])[n:43][cH:44][n:45][c:46]45)[C@H:47]([OH:48])[C@@H:49]3[OH:50])[C@@H:51]([OH:52])[C@H:53]2[OH:54])[CH:18]=[CH:17][CH2:16]1"
+
+def test_separating_mapped_rxn_str_into_reactant_and_product_strs_03():
+    rxn = reaction.mapped_reaction(rxn_smarts = mannofuranose_AdH_rxn_str_mapped)
+    reactants_str, products_str = rxn._rxn_2_cpds()
+    assert reactants_str == "[CH3:1][C@H:2]([OH:3])[C@@H:4]1[O:5][CH:6]([OH:7])[C@H:8]([OH:9])[C@@H:10]1[OH:11].[NH2:12][C:13](=[O:14])[c:15]1[cH:16][cH:17][cH:18][n+:19]([C@@H:20]2[O:21][C@H:22]([CH2:23][O:24][P:25](=[O:26])([OH:27])[O:28][P:29](=[O:30])([OH:31])[O:32][CH2:33][C@H:34]3[O:35][C@@H:36]([n:37]4[cH:38][n:39][c:40]5[c:41]([NH2:42])[n:43][cH:44][n:45][c:46]45)[C@H:47]([OH:48])[C@@H:49]3[OH:50])[C@@H:51]([OH:52])[C@H:53]2[OH:54])[cH:55]1"
+    assert products_str == "[CH3:1][C@H:2]([OH:3])[C@@H:4]1[O:5][C:6](=[O:7])[C@H:8]([OH:9])[C@@H:10]1[OH:11].[H+].[NH2:12][C:13](=[O:14])[C:15]1=[CH:55][N:19]([C@@H:20]2[O:21][C@H:22]([CH2:23][O:24][P:25](=[O:26])([OH:27])[O:28][P:29](=[O:30])([OH:31])[O:32][CH2:33][C@H:34]3[O:35][C@@H:36]([n:37]4[cH:38][n:39][c:40]5[c:41]([NH2:42])[n:43][cH:44][n:45][c:46]45)[C@H:47]([OH:48])[C@@H:49]3[OH:50])[C@@H:51]([OH:52])[C@H:53]2[OH:54])[CH:18]=[CH:17][CH2:16]1"
+
+def test_extracting_substrates_from_mapped_rxn_str_03(cofactors_list):
+    rxn = reaction.mapped_reaction(rxn_smarts = mannofuranose_AdH_rxn_str_mapped)
+    assert rxn.get_substrates(cofactors_list = cofactors_list,
+                              consider_stereo = False) == ["[CH3:1][C@H:2]([OH:3])[C@@H:4]1[O:5][CH:6]([OH:7])[C@H:8]([OH:9])[C@@H:10]1[OH:11]"]
+
+def test_extracting_lhs_cofactors_from_mapped_rxn_str_03(cofactors_list):
+    rxn = reaction.mapped_reaction(rxn_smarts = mannofuranose_AdH_rxn_str_mapped)
+    assert rxn.get_lhs_cofactors(cofactors_list = cofactors_list,
+                                 consider_stereo = False) == ["[NH2:12][C:13](=[O:14])[c:15]1[cH:16][cH:17][cH:18][n+:19]([C@@H:20]2[O:21][C@H:22]([CH2:23][O:24][P:25](=[O:26])([OH:27])[O:28][P:29](=[O:30])([OH:31])[O:32][CH2:33][C@H:34]3[O:35][C@@H:36]([n:37]4[cH:38][n:39][c:40]5[c:41]([NH2:42])[n:43][cH:44][n:45][c:46]45)[C@H:47]([OH:48])[C@@H:49]3[OH:50])[C@@H:51]([OH:52])[C@H:53]2[OH:54])[cH:55]1"]
+
+def test_extracting_rhs_cofactors_from_mapped_rxn_str_03(cofactors_list):
+    rxn = reaction.mapped_reaction(rxn_smarts = mannofuranose_AdH_rxn_str_mapped)
+    assert rxn.get_rhs_cofactors(cofactors_list = cofactors_list,
+                                 consider_stereo = False) == ["[H+]", "[NH2:12][C:13](=[O:14])[C:15]1=[CH:55][N:19]([C@@H:20]2[O:21][C@H:22]([CH2:23][O:24][P:25](=[O:26])([OH:27])[O:28][P:29](=[O:30])([OH:31])[O:32][CH2:33][C@H:34]3[O:35][C@@H:36]([n:37]4[cH:38][n:39][c:40]5[c:41]([NH2:42])[n:43][cH:44][n:45][c:46]45)[C@H:47]([OH:48])[C@@H:49]3[OH:50])[C@@H:51]([OH:52])[C@H:53]2[OH:54])[CH:18]=[CH:17][CH2:16]1"]
+
+
+#
 def test_get_mapped_bonds_data_type():
     """
     Test to ensure the internal function _get_mapped_bonds returns a set.
