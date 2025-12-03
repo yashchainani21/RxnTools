042968042c8723e088ddab64db330f4aad0c87a6
yashchainani21 <ychainani21@gmail.com>
2025-12-03 16:15:48 -0600

    address RDKit warnings

--- scripts/01_post_process_JN_rule_mapping.py
+++ scripts/01_post_process_JN_rule_mapping.py
@@ -108,3 +108,4 @@ final_df = input_rxns_w_JN_mappings_df.iloc[keep_idx, :].copy()
 final_df['top_mapped_operator'] = all_top_mapped_operators
 
 print(rxns_skipped_count, "reactions were skipped due to errors.")
+print(f"Final processed reactions count: {final_df.shape[0]}")
