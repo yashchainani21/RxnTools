df7ff6101f1272c23abc5aa10a025ac9a7231b8c
yashchainani21 <ychainani21@gmail.com>
2025-12-05 15:23:26 -0600

    check rxns count

--- tests/test_processed_datasets.py
+++ tests/test_processed_datasets.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 def test_processed_KEGG_rxns_not_empty():
     KEGG_df = pd.read_parquet("../data/processed/enzymemap_Kegg_JN_mapped_non_unique.parquet")
-    assert KEGG_df.shape[0] > 0
+    assert KEGG_df.shape[0] == 7966
     assert 'top_mapped_operator' in KEGG_df.columns
 
 def test_processed_MetaCyc_rxns_not_empty():
     MetaCyc_df = pd.read_parquet("../data/processed/enzymemap_MetaCyc_JN_mapped_non_unique.parquet")
-    assert MetaCyc_df.shape[0] > 0
+    assert MetaCyc_df.shape[0] == 4580 # confirmed against the original .csv file too
     assert 'top_mapped_operator' in MetaCyc_df.columns
 
 def test_processed_KEGG_rule0002_and_rule0003_rxns_count():
