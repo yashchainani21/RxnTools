c5a3b4fca3aefe677b9c00a3e76ce3e7d3b87df3
yashchainani21 <ychainani21@gmail.com>
2025-12-05 14:56:51 -0600

    expand tests

--- tests/test_processed_datasets.py
+++ tests/test_processed_datasets.py
@@ -6,3 +6,7 @@ def test_processed_KEGG_rxns_not_empty():
     assert KEGG_df.shape[0] > 0
     assert 'top_mapped_operator' in KEGG_df.columns
 
+def test_processed_MetaCyc_rxns_not_empty():
+    MetaCyc_df = pd.read_parquet("../data/processed/enzymemap_MetaCyc_JN_mapped_non_unique.parquet")
+    assert MetaCyc_df.shape[0] > 0
+    assert 'top_mapped_operator' in MetaCyc_df.columns
