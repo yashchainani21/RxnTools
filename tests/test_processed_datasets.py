2c0ba571e35195f0cf353c73f4ba8c620c0f7c90
yashchainani21 <ychainani21@gmail.com>
2025-12-08 20:07:33 -0600

    test mapped decarboxylation and carboxylation rxns

--- tests/test_processed_datasets.py
+++ tests/test_processed_datasets.py
@@ -118,6 +118,9 @@ def test_processed_KEGG_rule0023_and_rule0024_rxns_count(KEGG_df):
     # the rhs cofactors column for decarboxylation rxns (rule0024) should contain exactly one element each
     assert rule0024_df['RHS_cofactors'].apply(lambda x: isinstance(x, np.ndarray) and len(x) == 1).all()
 
+    # the lhs cofactor codes column for carboxylation rxns (rule0023) should contain exactly one element: CO2_CoF
+    assert rule0023_df['LHS_cofactor_codes'].apply(lambda x: isinstance(x, np.ndarray) and len(x) == 1 and x[0] == 'CO2').all()
+
 # test decarboxylase related rules for MetaCyc were mapped correctly
 def test_processed_MetaCyc_rule0023_and_rule0024_rxns_count(MetaCyc_df):
     rule0023_df = MetaCyc_df[MetaCyc_df['top_mapped_operator'] == 'rule0023']
